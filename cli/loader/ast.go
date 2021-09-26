4c47ef9725057db972b3b88e4dbc385e48527d1c
switchupcb <contact@switchupcb.com>
2021-09-25 20:49:16 -0500

    refactor ast
    removes unrequired functions

--- cli/loader/ast.go
+++ cli/loader/ast.go
@@ -3,16 +3,11 @@ package loader
 import (
 	"fmt"
 	"go/ast"
-	"go/build"
 	"go/importer"
 	"go/parser"
 	"go/token"
 	"go/types"
-	"io/fs"
-	"os"
-	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/switchupcb/copygen/cli/models"
 	"golang.org/x/tools/go/packages"
@@ -89,6 +84,75 @@ func astTypeSearch(f *ast.File, t string) (*ast.TypeSpec, error) {
 		}
 	}
 	return nil, fmt.Errorf("The type %v could not be found in the AST.", t)
+}
+
+// astDepthFields finds fields of fields using an AST.
+func astSelectorSearch(f *ast.File, ts *ast.TypeSpec, selector string) *ast.SelectorExpr {
+	if strct, ok := ts.Type.(*ast.StructType); ok {
+		for _, field := range strct.Fields.List {
+			if sel, ok := field.Type.(*ast.SelectorExpr); ok {
+				fieldTypePkg := sel.X.(*ast.Ident).Name // 'log' in 'Field log.Logger'
+				fieldTypeName := sel.Sel.Name           // 'Logger' in 'Field log.Logger'
+				if selector == fieldTypePkg+"."+fieldTypeName {
+					return sel
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// astLocateType finds the import path, package, and typename of a type in an AST.
+func astLocateType(f *ast.File, fld *ast.SelectorExpr) (string, string, string) {
+	fldTypePkg := fld.X.(*ast.Ident).Name // 'log' in 'Field log.Logger'
+	fldTypeName := fld.Sel.Name           // 'Logger' in 'Field log.Logger'
+
+	// don't alter the original file's slice
+	var checkedImprts []*ast.ImportSpec
+	for _, v := range f.Imports {
+		checkedImprts = append(checkedImprts, v)
+	}
+
+	// check imports that have variable names
+	for i := len(checkedImprts) - 1; i >= 0; i-- {
+		imprt := checkedImprts[i]
+
+		// if an import has a variable name
+		if imprt.Name != nil {
+			// if an import variable matches the package name (i.e 'log' in 'log.Logger')
+			if fldTypePkg == imprt.Name.Name {
+				return imprt.Path.Value, fldTypePkg, fldTypeName
+			} else {
+				// remove
+				checkedImprts = checkedImprts[:len(checkedImprts)-1]
+			}
+		}
+	}
+
+	// check remaining imports (that don't have variable names)
+	for _, imprt := range checkedImprts {
+		imprtPath := imprt.Path.Value
+		imprtPath = imprtPath[1 : len(imprtPath)-1] // "log" -> log
+		if fldTypePkg == imprtPath {
+			return imprtPath, fldTypePkg, fldTypeName
+		}
+	}
+	return "", fldTypePkg, fldTypeName
+}
+
+func isBasic(t types.Type) bool {
+	switch x := t.(type) {
+	case *types.Basic:
+		return true
+	case *types.Slice:
+		return true
+	case *types.Map:
+		return true
+	case *types.Pointer:
+		return isBasic(x.Elem())
+	default:
+		return false
+	}
 }
 
 // FieldSearch represents a search for a field.
@@ -168,75 +232,6 @@ func astFieldSearch(info types.Info, f *ast.File, ts *ast.TypeSpec, imprt string
 	}
 }
 
-// astDepthFields finds fields of fields using an AST.
-func astSelectorSearch(f *ast.File, ts *ast.TypeSpec, selector string) *ast.SelectorExpr {
-	if strct, ok := ts.Type.(*ast.StructType); ok {
-		for _, field := range strct.Fields.List {
-			if sel, ok := field.Type.(*ast.SelectorExpr); ok {
-				fieldTypePkg := sel.X.(*ast.Ident).Name // 'log' in 'Field log.Logger'
-				fieldTypeName := sel.Sel.Name           // 'Logger' in 'Field log.Logger'
-				if selector == fieldTypePkg+"."+fieldTypeName {
-					return sel
-				}
-			}
-		}
-	}
-	return nil
-}
-
-// astLocateType finds the import path, package, and typename of a type in an AST.
-func astLocateType(f *ast.File, fld *ast.SelectorExpr) (string, string, string) {
-	fldTypePkg := fld.X.(*ast.Ident).Name // 'log' in 'Field log.Logger'
-	fldTypeName := fld.Sel.Name           // 'Logger' in 'Field log.Logger'
-
-	// don't alter the original file's slice
-	var checkedImprts []*ast.ImportSpec
-	for _, v := range f.Imports {
-		checkedImprts = append(checkedImprts, v)
-	}
-
-	// check imports that have variable names
-	for i := len(checkedImprts) - 1; i >= 0; i-- {
-		imprt := checkedImprts[i]
-
-		// if an import has a variable name
-		if imprt.Name != nil {
-			// if an import variable matches the package name (i.e 'log' in 'log.Logger')
-			if fldTypePkg == imprt.Name.Name {
-				return imprt.Path.Value, fldTypePkg, fldTypeName
-			} else {
-				// remove
-				checkedImprts = checkedImprts[:len(checkedImprts)-1]
-			}
-		}
-	}
-
-	// check remaining imports (that don't have variable names)
-	for _, imprt := range checkedImprts {
-		imprtPath := imprt.Path.Value
-		imprtPath = imprtPath[1 : len(imprtPath)-1] // "log" -> log
-		if fldTypePkg == imprtPath {
-			return imprtPath, fldTypePkg, fldTypeName
-		}
-	}
-	return "", fldTypePkg, fldTypeName
-}
-
-func isBasic(t types.Type) bool {
-	switch x := t.(type) {
-	case *types.Basic:
-		return true
-	case *types.Slice:
-		return true
-	case *types.Map:
-		return true
-	case *types.Pointer:
-		return isBasic(x.Elem())
-	default:
-		return false
-	}
-}
-
 // printFields shows a tree of fields for a given type.
 func PrintFields(t string, fields []models.Field, tabs string) {
 	if tabs == "" {
@@ -250,120 +245,4 @@ func PrintFields(t string, fields []models.Field, tabs string) {
 			PrintFields(field.Definition, field.Fields, tabs+"\t")
 		}
 	}
-}
-
-// astFunctionSig parses an package to provide a function signature.
-func astFunctionSig(m *ast.Field) string {
-	fn := "func ("
-
-	params := m.Type.(*ast.FuncType).Params.List
-	fmt.Println(params)
-	var plist []string
-	for i := 0; i < len(params); i++ {
-		fmt.Println("PARAM")
-		fmt.Println(params[i].Type.(*ast.Ident).Name)
-		plist = append(plist, params[i].Type.(*ast.Ident).Name)
-	}
-	if len(plist) > 0 {
-		pstring := strings.Join(plist, ", ")
-		fn += pstring[:len(pstring)-2]
-	}
-	fn += ") "
-
-	results := m.Type.(*ast.FuncType).Results.List
-	var rlist []string
-	for i := 0; i < len(results); i++ {
-		if results[i].Names != nil {
-			rlist = append(rlist, results[i].Names[0].Name)
-		}
-	}
-	rstring := strings.Join(rlist, ", ")
-	if len(rstring) > 2 {
-		rstring = rstring[:len(rstring)-2]
-	}
-
-	if len(rlist) > 1 {
-		fn += "("
-		fn += rstring
-		fn += ")"
-	} else {
-		fn += rstring
-	}
-	return fn
-}
-
-// getPackageFiles uses the GOPATH to find the absolute path of .go files from a specific package in a library.
-func getPackageFiles(imprt string) ([]string, error) {
-	var gofiles []string
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		gopath = build.Default.GOPATH
-	}
-
-	absgopath, err := filepath.Abs(gopath)
-	if err != nil {
-		return nil, fmt.Errorf("There was an error retrieving the absolute filepath for GOPATH.")
-	}
-
-	// The pkg directory contains Go package objects compiled from src directory Go source code packages.
-	absfilepath := filepath.Join(absgopath, "pkg/mod")
-
-	// libraries are stored with a hash (i.e `copygen@...`) and can contain multiple versions.
-	importDirs := strings.Split(imprt, "/")
-	fmt.Println(absfilepath)
-	fmt.Println(importDirs)
-
-	// find the package@... folders
-	var i int
-	for i = 0; i < len(importDirs); i++ {
-		newpath := filepath.Join(absfilepath, importDirs[i])
-		if _, err := os.Stat(newpath); err != nil {
-			break
-		}
-		absfilepath = newpath
-	}
-
-	// find the latest library hash
-	if i != len(importDirs)-1 {
-		files, err := os.ReadDir(absfilepath)
-		if err != nil {
-			return nil, fmt.Errorf("An error occurred finding an import package. Is there a go module in?: %v\n%v.", absfilepath, err)
-		}
-
-		var modTime time.Time
-		var module string
-		for _, file := range files {
-			if file.IsDir() && strings.Contains(file.Name(), importDirs[i]) {
-				fileInfo, err := file.Info()
-				if err != nil {
-					return nil, fmt.Errorf("An error occurred retrieving file info for an import package: %v\n%v.", absfilepath, err)
-				}
-
-				if fileInfo.ModTime().After(modTime) {
-					modTime = fileInfo.ModTime()
-					module = file.Name()
-				}
-			}
-		}
-		// finalize the package path
-		absfilepath = filepath.Join(absfilepath, module)
-		if i+1 < len(importDirs) {
-			absfilepath = filepath.Join(absfilepath, filepath.Join((importDirs[i+1:])...))
-		}
-	}
-
-	// find .go files in the specified package.
-	filter := func(path string, d fs.DirEntry, err error) error {
-		if filepath.Ext(path) == ".go" {
-			gofiles = append(gofiles, path)
-		}
-		return nil
-	}
-
-	if err = filepath.WalkDir(absfilepath, filter); err != nil {
-		return nil, fmt.Errorf("There was an error searching through an imported library: %v", err)
-	} else if len(gofiles) == 0 {
-		return nil, fmt.Errorf("No .go files were found in the specified package: %v\nIs the module up to date?", absfilepath)
-	}
-	return gofiles, nil
 }
