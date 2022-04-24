c1bb869f251fa6261b5bcbf6c33b02d614f0e592
switchupcb <contact@switchupcb.com>
2022-04-23 19:34:58 -0500

    remove linter comment

--- cli/generator/interpreter/extract/extract.go
+++ cli/generator/interpreter/extract/extract.go
@@ -4,6 +4,6 @@ package extract
 import "reflect"
 
 // Symbols are extracted from the internal types (compiled at runtime).
-var Symbols = make(map[string]map[string]reflect.Value) // nolint:gochecknoglobals // yaegi
+var Symbols = make(map[string]map[string]reflect.Value)
 
 //go:generate yaegi extract github.com/switchupcb/copygen/cli/models
