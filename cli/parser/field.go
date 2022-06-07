switchupcb/copygen

commit 546714bfde02d01c10ea1e859bc58733ef45095d

fix collection imports in parser
fixes #27

cli/parser/field.go
@@ -50,23 +50,26 @@ func parseField(typ types.Type) *models.Field {
 	case *types.Pointer:
 		elemfield := parseField(x.Elem())
 
-		// type aliases (including structs) must be deepcopied.
+		// type aliases (including structs) must be deepcopied
+		// in order to match underlying fields.
 		if elemfield.IsAlias() {
-			field = elemfield.Deepcopy(nil)
+			deepfield := elemfield.Deepcopy(nil)
+			field.Fields = deepfield.Fields
 		}
-		field.Definition = models.CollectionPointer + elemfield.Definition
+
+		field.Definition = models.CollectionPointer + collectedDefinition(elemfield)
 
 	case *types.Array:
-		field.Definition = "[" + fmt.Sprint(x.Len()) + "]" + parseField(x.Elem()).Definition
+		field.Definition = "[" + fmt.Sprint(x.Len()) + "]" + collectedDefinition(parseField(x.Elem()))
 
 	case *types.Slice:
-		field.Definition = models.CollectionSlice + parseField(x.Elem()).Definition
+		field.Definition = models.CollectionSlice + collectedDefinition(parseField(x.Elem()))
 
 	case *types.Map:
-		field.Definition = models.CollectionMap + "[" + parseField(x.Key()).Definition + "]" + parseField(x.Elem()).Definition
+		field.Definition = models.CollectionMap + "[" + collectedDefinition(parseField(x.Key())) + "]" + collectedDefinition(parseField(x.Elem()))
 
 	case *types.Chan:
-		field.Definition = models.CollectionChan + " " + parseField(x.Elem()).Definition
+		field.Definition = models.CollectionChan + " " + collectedDefinition(parseField(x.Elem()))
 
 	// Function (without Receivers)
 	// https://go.googlesource.com/example/+/HEAD/gotypes#function-and-method-types
@@ -76,7 +79,7 @@ func parseField(typ types.Type) *models.Field {
 		// set the parameters.
 		definition.WriteString(models.CollectionFunc + "(")
 		for i := 0; i < x.Params().Len(); i++ {
-			definition.WriteString(parseField(x.Params().At(i).Type()).Definition)
+			definition.WriteString(collectedDefinition(parseField(x.Params().At(i).Type())))
 			if i+1 != x.Params().Len() {
 				definition.WriteString(", ")
 			}
@@ -91,7 +94,7 @@ func parseField(typ types.Type) *models.Field {
 			definition.WriteString("(")
 		}
 		for i := 0; i < x.Results().Len(); i++ {
-			definition.WriteString(parseField(x.Results().At(i).Type()).Definition)
+			definition.WriteString(collectedDefinition(parseField(x.Results().At(i).Type())))
 			if i+1 != x.Results().Len() {
 				definition.WriteString(", ")
 			}
@@ -111,7 +114,7 @@ func parseField(typ types.Type) *models.Field {
 			var definition strings.Builder
 			definition.WriteString(models.CollectionInterface + "{")
 			for i := 0; i < x.NumMethods(); i++ {
-				definition.WriteString(parseField(x.Method(i).Type()).Definition + "; ")
+				definition.WriteString(collectedDefinition(parseField(x.Method(i).Type())) + "; ")
 			}
 			definition.WriteString("}")
 			field.Definition = definition.String()
@@ -130,7 +133,7 @@ func parseField(typ types.Type) *models.Field {
 			setTags(subfield, x.Tag(i))
 			subfield.Parent = field
 			field.Fields = append(field.Fields, subfield)
-			definition.WriteString(subfield.Name + " " + subfield.Definition + "; ")
+			definition.WriteString(subfield.Name + " " + subfield.FullDefinition() + "; ")
 
 			// all subfields are deepcopied with Fields[0].
 			//
@@ -165,14 +168,6 @@ func setFieldImportAndPackage(field *models.Field, pkg *types.Package) {
 
 	field.Import = pkg.Path()
 	field.Package = pkg.Name()
-
-	// field collections set collected types' packages in the field.Definition.
-	// i.e map[*domain.Account]string
-	if field.IsCollection() {
-		field.Definition = field.Package + "." + field.Definition
-		field.Import = ""
-		field.Package = ""
-	}
 }
 
 // setTags sets the tags for a field.
@@ -189,4 +184,21 @@ func setTags(field *models.Field, rawtag string) {
 			tag.Name: tag.Options,
 		}
 	}
+}
+
+// collectedDefinition determines the full definition for a collected type in a collection.
+//
+// collectedDefinition can be called in the parser, but ONLY because collections are NOT cached.
+func collectedDefinition(collected *models.Field) string {
+	// a generated file's package == setup file's package.
+	//
+	// when the field is defined in the setup file (i.e `Collection`),
+	// it will be parsed with the setup file's package (i.e `copygen.Collection`).
+	//
+	// do NOT reference it by package in the generated file (i.e `Collection`).
+	if collected.Import == setupPkgPath {
+		return collected.Definition
+	}
+
+	return collected.FullDefinition()
 }

cli/parser/parse.go
@@ -48,6 +48,15 @@ var (
 	// fieldcache improves performance by parsing a unique type definition once per runtime.
 	// definitions remain constant UNLESS the user modifies their modules during runtime.
 	fieldcache map[string]*models.Field
+
+	// setupPkgPath represents the current path of the setup file's package.
+	//
+	// setupPkgPath is used to remove package references from collected types in collections
+	// that will be used in the generated file's package (equal to the setup file's package).
+	//
+	// i.e `Collections` parsed as `copygen.Collections` in the setup file's package copygen,
+	// output as `Collections` in the generated file's package copygen.
+	setupPkgPath string
 )
 
 // SetupCache sets up the parser's global cache.
@@ -87,6 +96,7 @@ func Parse(gen *models.Generator) error {
 	if err != nil {
 		return fmt.Errorf("an error occurred while loading the packages for types.\n%w", err)
 	}
+	setupPkgPath = p.Pkgs[0].PkgPath
 
 	// find a new instance of a copygen AST since the old one has its comments removed.
 	var newCopygen *ast.InterfaceType
@@ -121,6 +131,9 @@ func Parse(gen *models.Generator) error {
 	}
 	gen.Keep = buf.Bytes()
 
+	// reset global variables.
+	setupPkgPath = ""
+
 	return nil
 }
 
@@ -133,7 +146,6 @@ func (p *Parser) setPackages(gen *models.Generator) {
 	if len(pkgs) > 0 {
 		outputPkgPath = pkgs[0].PkgPath
 	}
-	setupPkgPath := p.Pkgs[0].PkgPath
 
 	// adjust the packages of each field where necessary.
 	imports := p.Config.SetupFile.Imports
@@ -154,10 +166,12 @@ func (p *Parser) setPackages(gen *models.Generator) {
 			for _, t := range types {
 				for _, field := range t.Field.AllFields(nil, nil) {
 
-					// a generated file's package == setup file's package
+					// a generated file's package == setup file's package.
 					//
 					// when the field is defined in the setup file (i.e `Collection`),
-					// do NOT reference it by package in the generated file.
+					// it will be parsed with the setup file's package (i.e `copygen.Collection`).
+					//
+					// do NOT reference it by package in the generated file (i.e `Collection`).
 					if field.Import == setupPkgPath {
 						field.Package = ""
 						continue

examples/_tests/multi/copygen.go
@@ -41,21 +41,21 @@ func Array(tb [16]byte, fb [16]byte) {
 }
 
 // ArrayComplex copies a [16]map[byte]string to a *complex.Collection.
-func ArrayComplex(tC *complex.Collection, fm [16]map[byte]string) {
+func ArrayComplex(tc *complex.Collection, fm [16]map[byte]string) {
 	// *complex.Collection fields
-	tC.Arr = fm
+	tc.Arr = fm
 }
 
-// ArrayExternal copies a [16]Placeholder to a [16]Placeholder.
-func ArrayExternal(tP [16]Placeholder, fP [16]Placeholder) {
-	// [16]Placeholder fields
-	tP = fP
+// ArrayExternal copies a [16]external.Placeholder to a [16]external.Placeholder.
+func ArrayExternal(te [16]external.Placeholder, fe [16]external.Placeholder) {
+	// [16]external.Placeholder fields
+	te = fe
 }
 
-// ArrayExternalComplex copies a [16]map[*Collection]string to a *complex.ComplexCollection.
-func ArrayExternalComplex(tC *complex.ComplexCollection, fm [16]map[*Collection]string) {
+// ArrayExternalComplex copies a [16]map[*external.Collection]string to a *complex.ComplexCollection.
+func ArrayExternalComplex(tc *complex.ComplexCollection, fm [16]map[*external.Collection]string) {
 	// *complex.ComplexCollection fields
-	tC.Arr = fm
+	tc.Arr = fm
 }
 
 // ArraySimple copies a [16]byte to a *Collection.
@@ -77,18 +77,18 @@ func BasicDoublePointer(tb **bool, fb *bool) {
 }
 
 // BasicExternal copies a *external.Placeholder to a external.Placeholder.
-func BasicExternal(tP external.Placeholder, fP *external.Placeholder) {
+func BasicExternal(tP external.Placeholder, fe *external.Placeholder) {
 	// external.Placeholder fields
-	tP = *fP
+	tP = *fe
 }
 
 // BasicExternalMulti copies a *external.Placeholder to a external.Placeholder, *external.Placeholder.
-func BasicExternalMulti(tP external.Placeholder, tP1 *external.Placeholder, fP *external.Placeholder) {
+func BasicExternalMulti(tP external.Placeholder, te *external.Placeholder, fe *external.Placeholder) {
 	// external.Placeholder fields
-	tP = *fP
+	tP = *fe
 
 	// *external.Placeholder fields
-	tP1 = fP
+	te = fe
 }
 
 // BasicPointer copies a bool to a *bool.
@@ -126,19 +126,19 @@ func Chan(tc chan int, fc chan int) {
 }
 
 // ChanComplex copies a chan *[]int to a *complex.Collection.
-func ChanComplex(tC *complex.Collection, fc chan *[]int) {
+func ChanComplex(tc *complex.Collection, fc chan *[]int) {
 	// *complex.Collection fields
-	tC.C = fc
+	tc.C = fc
 }
 
-// ChanExternal copies a chan Placeholder to a chan Placeholder.
-func ChanExternal(tc chan Placeholder, fc chan Placeholder) {
-	// chan Placeholder fields
+// ChanExternal copies a chan external.Placeholder to a chan external.Placeholder.
+func ChanExternal(tc chan external.Placeholder, fc chan external.Placeholder) {
+	// chan external.Placeholder fields
 	tc = fc
 }
 
-// ChanExternalComplex copies a chan *[]Collection to a complex.ComplexCollection.
-func ChanExternalComplex(tC complex.ComplexCollection, fc chan *[]Collection) {
+// ChanExternalComplex copies a chan *[]external.Collection to a complex.ComplexCollection.
+func ChanExternalComplex(tC complex.ComplexCollection, fc chan *[]external.Collection) {
 	// complex.ComplexCollection fields
 	tC.C = fc
 }
@@ -161,21 +161,21 @@ func Func(tf func() int, ff func() int) {
 }
 
 // FuncComplex copies a func([]string, uint64) *byte to a *complex.Collection.
-func FuncComplex(tC *complex.Collection, ff func([]string, uint64) *byte) {
+func FuncComplex(tc *complex.Collection, ff func([]string, uint64) *byte) {
 	// *complex.Collection fields
-	tC.F = ff
+	tc.F = ff
 }
 
-// FuncExternal copies a func(Placeholder) int to a func(Placeholder) int.
-func FuncExternal(tf func(Placeholder) int, ff func(Placeholder) int) {
-	// func(Placeholder) int fields
+// FuncExternal copies a func(external.Placeholder) int to a func(external.Placeholder) int.
+func FuncExternal(tf func(external.Placeholder) int, ff func(external.Placeholder) int) {
+	// func(external.Placeholder) int fields
 	tf = ff
 }
 
-// FuncExternalComplex copies a func(Collection) []string to a *complex.ComplexCollection.
-func FuncExternalComplex(tC *complex.ComplexCollection, ff func(Collection) []string) {
+// FuncExternalComplex copies a func(external.Collection) []string to a *complex.ComplexCollection.
+func FuncExternalComplex(tc *complex.ComplexCollection, ff func(external.Collection) []string) {
 	// *complex.ComplexCollection fields
-	tC.F = ff
+	tc.F = ff
 }
 
 // FuncSimple copies a func() int to a *Collection.
@@ -191,20 +191,20 @@ func Interface(ti interface{}, fi interface{}) {
 }
 
 // InterfaceComplex copies a interface{func(rune) *int; } to a *complex.Collection.
-func InterfaceComplex(tC *complex.Collection, fi interface{ func(rune) *int }) {
+func InterfaceComplex(tc *complex.Collection, fi interface{ func(rune) *int }) {
 	// *complex.Collection fields
-	tC.I = fi
+	tc.I = fi
 }
 
 // InterfaceExternal copies a error to a *external.Collection.
-func InterfaceExternal(tC *external.Collection, fe error) {
+func InterfaceExternal(te *external.Collection, fe error) {
 	// *external.Collection fields
-	tC.I = fe
+	te.I = fe
 }
 
-// InterfaceExternalComplex copies a interface{func(string) map[*Collection]bool; func() (int, byte); } to a complex.ComplexCollection.
+// InterfaceExternalComplex copies a interface{func(string) map[*external.Collection]bool; func() (int, byte); } to a complex.ComplexCollection.
 func InterfaceExternalComplex(tC complex.ComplexCollection, fi interface {
-	func(string) map[*Collection]bool
+	func(string) map[*external.Collection]bool
 	func() (int, byte)
 }) {
 	// complex.ComplexCollection fields
@@ -224,21 +224,21 @@ func Map(tm map[string]bool, fm map[string]bool) {
 }
 
 // MapComplex copies a map[string]interface{func() string; } to a *complex.Collection.
-func MapComplex(tC *complex.Collection, fm map[string]interface{ func() string }) {
+func MapComplex(tc *complex.Collection, fm map[string]interface{ func() string }) {
 	// *complex.Collection fields
-	tC.M = fm
+	tc.M = fm
 }
 
-// MapExternal copies a map[string]Placeholder to a map[string]Placeholder.
-func MapExternal(tm map[string]Placeholder, fm map[string]Placeholder) {
-	// map[string]Placeholder fields
+// MapExternal copies a map[string]external.Placeholder to a map[string]external.Placeholder.
+func MapExternal(tm map[string]external.Placeholder, fm map[string]external.Placeholder) {
+	// map[string]external.Placeholder fields
 	tm = fm
 }
 
-// MapExternalComplex copies a map[*Collection]Placeholder to a *complex.ComplexCollection.
-func MapExternalComplex(tC *complex.ComplexCollection, fm map[*Collection]Placeholder) {
+// MapExternalComplex copies a map[*external.Collection]external.Placeholder to a *complex.ComplexCollection.
+func MapExternalComplex(tc *complex.ComplexCollection, fm map[*external.Collection]external.Placeholder) {
 	// *complex.ComplexCollection fields
-	tC.M = fm
+	tc.M = fm
 }
 
 // MapSimple copies a map[string]bool to a *Collection.
@@ -258,7 +258,7 @@ func NoMatchBasic(tP Placeholder, fP Placeholder) {
 }
 
 // NoMatchBasicExternal copies a *Placeholder to a external.Placeholder, *external.Placeholder, bool.
-func NoMatchBasicExternal(tP external.Placeholder, tP1 *external.Placeholder, tb bool, fP *Placeholder) {
+func NoMatchBasicExternal(tP external.Placeholder, te *external.Placeholder, tb bool, fP *Placeholder) {
 	// external.Placeholder fields
 
 	// *external.Placeholder fields
@@ -271,9 +271,9 @@ func NoMatchChan(tC Collection, fc chan int) {
 	// Collection fields
 }
 
-// NoMatchComplex copies a []Collection to a []Collection.
-func NoMatchComplex(tC []Collection, fC []Collection) {
-	// []Collection fields
+// NoMatchExternal copies a []external.Collection to a []external.Collection.
+func NoMatchExternal(te []external.Collection, fe []external.Collection) {
+	// []external.Collection fields
 }
 
 // NoMatchFunc copies a func() int to a Collection.
@@ -303,21 +303,21 @@ func Slice(ts []string, fs []string) {
 }
 
 // SliceComplex copies a []map[string][16]int to a *complex.Collection.
-func SliceComplex(tC *complex.Collection, fm []map[string][16]int) {
+func SliceComplex(tc *complex.Collection, fm []map[string][16]int) {
 	// *complex.Collection fields
-	tC.S = fm
+	tc.S = fm
 }
 
-// SliceExternal copies a []Placeholder to a []Placeholder.
-func SliceExternal(tP []Placeholder, fP []Placeholder) {
-	// []Placeholder fields
-	tP = fP
+// SliceExternal copies a []external.Placeholder to a []external.Placeholder.
+func SliceExternal(te []external.Placeholder, fe []external.Placeholder) {
+	// []external.Placeholder fields
+	te = fe
 }
 
-// SliceExternalComplex copies a []map[string]func(*Collection) string to a *complex.ComplexCollection.
-func SliceExternalComplex(tC *complex.ComplexCollection, fm []map[string]func(*Collection) string) {
+// SliceExternalComplex copies a []map[string]func(*external.Collection) string to a *complex.ComplexCollection.
+func SliceExternalComplex(tc *complex.ComplexCollection, fm []map[string]func(*external.Collection) string) {
 	// *complex.ComplexCollection fields
-	tC.S = fm
+	tc.S = fm
 }
 
 // SlicePointer copies a []*int to a []*int.

examples/_tests/multi/setup/setup.go
@@ -68,7 +68,7 @@ type Copygen interface {
 	FuncComplex(F func([]string, uint64) *byte) *complex.Collection
 	FuncExternalComplex(F func(external.Collection) []string) *complex.ComplexCollection
 
-	NoMatchComplex([]external.Collection) (Struct []external.Collection)
+	NoMatchExternal([]external.Collection) (Struct []external.Collection)
 	EmptyStruct(struct{}) empty
 	Struct(Collection) Collection
 	StructExternal(external.Collection) *Collection

commit 7b25c1c4964c6fb24418afce54fde473b335238d

fix alias regression in 546714b
also uses the underlying definition for variable names in pointer fields

cli/parser/field.go
@@ -58,6 +58,7 @@ func parseField(typ types.Type) *models.Field {
 		}
 
 		field.Definition = models.CollectionPointer + collectedDefinition(elemfield)
+		field.VariableName = "." + alphastring(elemfield.Definition)
 
 	case *types.Array:
 		field.Definition = "[" + fmt.Sprint(x.Len()) + "]" + collectedDefinition(parseField(x.Elem()))
@@ -198,6 +199,17 @@ func collectedDefinition(collected *models.Field) string {
 	// do NOT reference it by package in the generated file (i.e `Collection`).
 	if collected.Import == setupPkgPath {
 		return collected.Definition
+	}
+
+	// when a setup file imports the package it will output to,
+	// do NOT reference the fields defined in the output package, by package.
+	if outputPkgPath != "" && collected.Import == outputPkgPath {
+		return collected.Definition
+	}
+
+	// when a field's import uses an alias, reassign the package reference.
+	if aliasPkg, ok := aliasImportMap[collected.Import]; ok {
+		return aliasPkg + "." + collected.Definition
 	}
 
 	return collected.FullDefinition()

cli/parser/parse.go
@@ -51,12 +51,32 @@ var (
 
 	// setupPkgPath represents the current path of the setup file's package.
 	//
-	// setupPkgPath is used to remove package references from collected types in collections
-	// that will be used in the generated file's package (equal to the setup file's package).
+	// setupPkgPath is used to remove package references from types that will be
+	// used in the generated file's package (equal to the setup file's package).
+	//
+	// setupPkgPath is referenced while parsing collected type definitions for collection fields,
+	// and while setting package references for non-collection fields after parsing.
 	//
 	// i.e `Collections` parsed as `copygen.Collections` in the setup file's package copygen,
 	// output as `Collections` in the generated file's package copygen.
 	setupPkgPath string
+
+	// outputPkgPath represents the generated file's package path.
+	//
+	// outputPkgPath is used to remove package references from types that are imported
+	// (in the setup file) from the generated file's package.
+	//
+	// outputPkgPath is referenced while parsing collected type definitions for collection fields,
+	// and while setting package references for non-collection fields after parsing.
+	outputPkgPath string
+
+	// aliasImportMap represents a map of import paths to package names.
+	//
+	// aliasImportMap is used to assign the correct package reference to an aliased field.
+	//
+	// aliasImportMap is referenced while parsing collected type definitions for collection fields,
+	// and while setting package references for non-collection fields after parsing.
+	aliasImportMap map[string]string
 )
 
 // SetupCache sets up the parser's global cache.
@@ -98,6 +118,20 @@ func Parse(gen *models.Generator) error {
 	}
 	setupPkgPath = p.Pkgs[0].PkgPath
 
+	// determine the output file package path.
+	outputPkgs, _ := packages.Load(&packages.Config{Mode: packages.NeedName}, "file="+gen.Outpath)
+	if len(outputPkgs) > 0 {
+		outputPkgPath = outputPkgs[0].PkgPath
+	}
+
+	// set the aliasImportMap.
+	aliasImportMap = make(map[string]string, len(p.Config.SetupFile.Imports))
+	for _, imp := range p.Config.SetupFile.Imports {
+		if imp.Name != nil {
+			aliasImportMap[imp.Path.Value[1:len(imp.Path.Value)-1]] = imp.Name.Name
+		}
+	}
+
 	// find a new instance of a copygen AST since the old one has its comments removed.
 	var newCopygen *ast.InterfaceType
 	for _, decl := range p.Pkgs[0].Syntax[0].Decls {
@@ -120,8 +154,8 @@ func Parse(gen *models.Generator) error {
 		return fmt.Errorf("%w", err)
 	}
 
-	// rename fields' packages using imports.
-	p.setPackages(gen)
+	// rename non-collection fields' packages using imports.
+	setPackages(gen)
 
 	// Write the Keep.
 	buf := new(bytes.Buffer)
@@ -133,29 +167,14 @@ func Parse(gen *models.Generator) error {
 
 	// reset global variables.
 	setupPkgPath = ""
+	outputPkgPath = ""
+	aliasImportMap = nil
 
 	return nil
 }
 
-// setFieldPackage sets the packages for all fields in a generator using names from the setup file.
-func (p *Parser) setPackages(gen *models.Generator) {
-
-	// determine the output file package path.
-	var outputPkgPath string
-	pkgs, _ := packages.Load(&packages.Config{Mode: packages.NeedName}, "file="+gen.Outpath)
-	if len(pkgs) > 0 {
-		outputPkgPath = pkgs[0].PkgPath
-	}
-
-	// adjust the packages of each field where necessary.
-	imports := p.Config.SetupFile.Imports
-	aliasImportMap := make(map[string]string, len(imports))
-	for _, imp := range imports {
-		if imp.Name != nil {
-			aliasImportMap[imp.Path.Value[1:len(imp.Path.Value)-1]] = imp.Name.Name
-		}
-	}
-
+// setPackages sets the packages for all fields in a generator using names from the setup file.
+func setPackages(gen *models.Generator) {
 	for _, function := range gen.Functions {
 		functionTypes := [][]models.Type{
 			function.From,

cli/parser/type.go
@@ -37,7 +37,9 @@ func parseTypeField(vars *types.Tuple) []models.Type {
 	for i := 0; i < vars.Len(); i++ {
 		field := parseField(vars.At(i).Type()).Deepcopy(nil)
 		field.Name = vars.At(i).Name()
-		field.VariableName = "." + alphastring(field.Definition)
+		if !field.IsPointer() {
+			field.VariableName = "." + alphastring(field.Definition)
+		}
 		types[i] = models.Type{Field: field}
 	}
 

examples/_tests/multi/copygen.go
@@ -41,9 +41,9 @@ func Array(tb [16]byte, fb [16]byte) {
 }
 
 // ArrayComplex copies a [16]map[byte]string to a *complex.Collection.
-func ArrayComplex(tc *complex.Collection, fm [16]map[byte]string) {
+func ArrayComplex(tC *complex.Collection, fm [16]map[byte]string) {
 	// *complex.Collection fields
-	tc.Arr = fm
+	tC.Arr = fm
 }
 
 // ArrayExternal copies a [16]external.Placeholder to a [16]external.Placeholder.
@@ -53,9 +53,9 @@ func ArrayExternal(te [16]external.Placeholder, fe [16]external.Placeholder) {
 }
 
 // ArrayExternalComplex copies a [16]map[*external.Collection]string to a *complex.ComplexCollection.
-func ArrayExternalComplex(tc *complex.ComplexCollection, fm [16]map[*external.Collection]string) {
+func ArrayExternalComplex(tC *complex.ComplexCollection, fm [16]map[*external.Collection]string) {
 	// *complex.ComplexCollection fields
-	tc.Arr = fm
+	tC.Arr = fm
 }
 
 // ArraySimple copies a [16]byte to a *Collection.
@@ -77,18 +77,18 @@ func BasicDoublePointer(tb **bool, fb *bool) {
 }
 
 // BasicExternal copies a *external.Placeholder to a external.Placeholder.
-func BasicExternal(tP external.Placeholder, fe *external.Placeholder) {
+func BasicExternal(tP external.Placeholder, fP *external.Placeholder) {
 	// external.Placeholder fields
-	tP = *fe
+	tP = *fP
 }
 
 // BasicExternalMulti copies a *external.Placeholder to a external.Placeholder, *external.Placeholder.
-func BasicExternalMulti(tP external.Placeholder, te *external.Placeholder, fe *external.Placeholder) {
+func BasicExternalMulti(tP external.Placeholder, tP1 *external.Placeholder, fP *external.Placeholder) {
 	// external.Placeholder fields
-	tP = *fe
+	tP = *fP
 
 	// *external.Placeholder fields
-	te = fe
+	tP1 = fP
 }
 
 // BasicPointer copies a bool to a *bool.
@@ -126,9 +126,9 @@ func Chan(tc chan int, fc chan int) {
 }
 
 // ChanComplex copies a chan *[]int to a *complex.Collection.
-func ChanComplex(tc *complex.Collection, fc chan *[]int) {
+func ChanComplex(tC *complex.Collection, fc chan *[]int) {
 	// *complex.Collection fields
-	tc.C = fc
+	tC.C = fc
 }
 
 // ChanExternal copies a chan external.Placeholder to a chan external.Placeholder.
@@ -161,9 +161,9 @@ func Func(tf func() int, ff func() int) {
 }
 
 // FuncComplex copies a func([]string, uint64) *byte to a *complex.Collection.
-func FuncComplex(tc *complex.Collection, ff func([]string, uint64) *byte) {
+func FuncComplex(tC *complex.Collection, ff func([]string, uint64) *byte) {
 	// *complex.Collection fields
-	tc.F = ff
+	tC.F = ff
 }
 
 // FuncExternal copies a func(external.Placeholder) int to a func(external.Placeholder) int.
@@ -173,9 +173,9 @@ func FuncExternal(tf func(external.Placeholder) int, ff func(external.Placeholde
 }
 
 // FuncExternalComplex copies a func(external.Collection) []string to a *complex.ComplexCollection.
-func FuncExternalComplex(tc *complex.ComplexCollection, ff func(external.Collection) []string) {
+func FuncExternalComplex(tC *complex.ComplexCollection, ff func(external.Collection) []string) {
 	// *complex.ComplexCollection fields
-	tc.F = ff
+	tC.F = ff
 }
 
 // FuncSimple copies a func() int to a *Collection.
@@ -191,15 +191,15 @@ func Interface(ti interface{}, fi interface{}) {
 }
 
 // InterfaceComplex copies a interface{func(rune) *int; } to a *complex.Collection.
-func InterfaceComplex(tc *complex.Collection, fi interface{ func(rune) *int }) {
+func InterfaceComplex(tC *complex.Collection, fi interface{ func(rune) *int }) {
 	// *complex.Collection fields
-	tc.I = fi
+	tC.I = fi
 }
 
 // InterfaceExternal copies a error to a *external.Collection.
-func InterfaceExternal(te *external.Collection, fe error) {
+func InterfaceExternal(tC *external.Collection, fe error) {
 	// *external.Collection fields
-	te.I = fe
+	tC.I = fe
 }
 
 // InterfaceExternalComplex copies a interface{func(string) map[*external.Collection]bool; func() (int, byte); } to a complex.ComplexCollection.
@@ -224,9 +224,9 @@ func Map(tm map[string]bool, fm map[string]bool) {
 }
 
 // MapComplex copies a map[string]interface{func() string; } to a *complex.Collection.
-func MapComplex(tc *complex.Collection, fm map[string]interface{ func() string }) {
+func MapComplex(tC *complex.Collection, fm map[string]interface{ func() string }) {
 	// *complex.Collection fields
-	tc.M = fm
+	tC.M = fm
 }
 
 // MapExternal copies a map[string]external.Placeholder to a map[string]external.Placeholder.
@@ -236,9 +236,9 @@ func MapExternal(tm map[string]external.Placeholder, fm map[string]external.Plac
 }
 
 // MapExternalComplex copies a map[*external.Collection]external.Placeholder to a *complex.ComplexCollection.
-func MapExternalComplex(tc *complex.ComplexCollection, fm map[*external.Collection]external.Placeholder) {
+func MapExternalComplex(tC *complex.ComplexCollection, fm map[*external.Collection]external.Placeholder) {
 	// *complex.ComplexCollection fields
-	tc.M = fm
+	tC.M = fm
 }
 
 // MapSimple copies a map[string]bool to a *Collection.
@@ -258,7 +258,7 @@ func NoMatchBasic(tP Placeholder, fP Placeholder) {
 }
 
 // NoMatchBasicExternal copies a *Placeholder to a external.Placeholder, *external.Placeholder, bool.
-func NoMatchBasicExternal(tP external.Placeholder, te *external.Placeholder, tb bool, fP *Placeholder) {
+func NoMatchBasicExternal(tP external.Placeholder, tP1 *external.Placeholder, tb bool, fP *Placeholder) {
 	// external.Placeholder fields
 
 	// *external.Placeholder fields
@@ -303,9 +303,9 @@ func Slice(ts []string, fs []string) {
 }
 
 // SliceComplex copies a []map[string][16]int to a *complex.Collection.
-func SliceComplex(tc *complex.Collection, fm []map[string][16]int) {
+func SliceComplex(tC *complex.Collection, fm []map[string][16]int) {
 	// *complex.Collection fields
-	tc.S = fm
+	tC.S = fm
 }
 
 // SliceExternal copies a []external.Placeholder to a []external.Placeholder.
@@ -315,9 +315,9 @@ func SliceExternal(te []external.Placeholder, fe []external.Placeholder) {
 }
 
 // SliceExternalComplex copies a []map[string]func(*external.Collection) string to a *complex.ComplexCollection.
-func SliceExternalComplex(tc *complex.ComplexCollection, fm []map[string]func(*external.Collection) string) {
+func SliceExternalComplex(tC *complex.ComplexCollection, fm []map[string]func(*external.Collection) string) {
 	// *complex.ComplexCollection fields
-	tc.S = fm
+	tC.S = fm
 }
 
 // SlicePointer copies a []*int to a []*int.